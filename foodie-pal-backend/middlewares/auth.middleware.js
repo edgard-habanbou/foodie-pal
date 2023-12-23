014c22d488805c2495851df00eb61ec92e294656
edgard <edgard.habanbou@hotmail.com>
2023-12-23 14:32:14 +0200

    Backend: Added Middlewear

---
+++ foodie-pal-backend/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    res.status(403).send("Forbidden");
+  } else {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findOne({ email: decoded.email }).select(
+        "-password"
+      );
+      req.user = user;
+      next();
+    } catch (e) {
+      res.status(403).send("Forbidden");
+    }
+  }
+};
+
+module.exports = {
+  authMiddleware,
+};
