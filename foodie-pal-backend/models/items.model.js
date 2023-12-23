03fb228ea12177eef31546cee0785f0fc190bbcd
edgard <edgard.habanbou@hotmail.com>
2023-12-23 14:35:53 +0200

    Backend: Added items Model

---
+++ foodie-pal-backend/models/items.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
