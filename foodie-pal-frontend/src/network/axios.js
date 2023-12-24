886a9f3cdc4a3c37166c9f7679c9298e70e5eb38
edgard <edgard.habanbou@hotmail.com>
2023-12-24 11:07:47 +0200

    Frontend: Added UserApi

---
+++ foodie-pal-frontend/src/network/axios.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.headers = {
+  Authorization: localStorage.getItem("jwt"),
+  "Content-Type": "application/json",
+};
+
+class UserApi {
+  async registerUser(user) {
+    try {
+      const response = await axios.post("/auth/register", user);
+      return response.data;
+    } catch (error) {
+      return {
+        error: error.response.data,
+      };
+    }
+  }
+  async loginUser(user) {
+    try {
+      const response = await axios.post("/auth/login", user);
+      return response.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+  async uploadImage(image) {
+    try {
+      const response = await axios.post("/profile/upload", image);
+      return response.data;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+}
+
+export const userApi = new UserApi();
