e18ec88cc3b865a25b5d0559e9075d21f4ecf3ad
edgard <edgard.habanbou@hotmail.com>
2024-01-10 21:34:20 +0200

    Frontend: Added Cook Page Styles

--- foodie-pal-frontend/src/pages/Cook/index.jsx
+++ foodie-pal-frontend/src/pages/Cook/index.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import Header from "../../components/Header";
+import Nav from "../../components/Nav";
 function Cook() {
   const { id } = useParams();
   const recipes = JSON.parse(localStorage.getItem("recipes"));
   const selectedRecipe = recipes.find(
     (recipe) => recipe.id === parseInt(id, 10)
   );
-  return <div>ID: {selectedRecipe.id}</div>;
+  return (
+    <div className="flex ">
+      <div>
+        <Nav />
+      </div>
+      <div className="landing ">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default Cook;
