6efdd2bf0c3c5daa8455e23dc5118b5090564898
edgard <edgard.habanbou@hotmail.com>
2024-01-11 14:40:06 +0200

    Frontend: Added Recipe Title and Image

--- foodie-pal-frontend/src/pages/Chat/index.jsx
+++ foodie-pal-frontend/src/pages/Chat/index.jsx
@@ -6,6 +6,10 @@ import "./index.css";
 
 function Chat() {
   const { id } = useParams();
+  const recipes = JSON.parse(localStorage.getItem("recipes"));
+  const selectedRecipe = recipes.find(
+    (recipe) => recipe.id === parseInt(id, 10)
+  );
   return (
     <div className="flex background">
       <div>
@@ -13,6 +17,23 @@ function Chat() {
       </div>
       <div className="landing">
         <Header />
+        <div className="flex gap center instructions-wrapper">
+          <div>
+            <img
+              className="recipe-image-large"
+              src={selectedRecipe.imageUrl}
+              alt="recipeImage"
+            />
+          </div>
+          <div className="recipe-desc  ">
+            <b>
+              <p>{selectedRecipe.title}</p>
+            </b>
+          </div>
+        </div>
+        <div className="chat-wrapper flex center">
+          <div className="chat"></div>
+        </div>
       </div>
     </div>
   );
