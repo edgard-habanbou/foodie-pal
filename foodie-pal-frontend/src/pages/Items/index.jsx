f3fd110b5bf7328a6fafeeb4b9da52c3a12721ca
edgard <edgard.habanbou@hotmail.com>
2024-01-11 20:13:41 +0200

    Frontend: Added form to add items

--- foodie-pal-frontend/src/pages/Items/index.jsx
+++ foodie-pal-frontend/src/pages/Items/index.jsx
@@ -4,15 +4,19 @@ import Header from "../../components/Header";
 import SwiperVertical from "../../components/SwiperVertical";
 import Loading from "../../components/Loading";
 import { userApi } from "../../network/axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
 function Items() {
   const [items, setItems] = useState([]);
   const [Load, setLoading] = useState(false);
+
+  const handleCamerabtn = () => {};
 
   const handleGetItems = async () => {
     setLoading(true);
     try {
       const response = await userApi.getUser();
-      console.log(response);
+      setItems(response.items);
     } catch (error) {
       setLoading(false);
     }
@@ -28,6 +32,13 @@ function Items() {
       </div>
       <div className="landing">
         <Header />
+        <div className="flex margin gap">
+          <input type="text" className="input" placeholder="Item Name" />
+          <button className="btn">Add Item</button>
+          <button className="btn">
+            <FontAwesomeIcon icon={faCamera} />
+          </button>
+        </div>
         <SwiperVertical instructions={items} />
       </div>
       {Load && <Loading />}
