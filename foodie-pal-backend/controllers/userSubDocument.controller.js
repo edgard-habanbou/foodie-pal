c300b00e6d0e698e19138c1ffaf9d98cdaa90302
edgard <edgard.habanbou@hotmail.com>
2024-01-14 14:13:19 +0200

    Backend: Added validation to check if item exists

--- foodie-pal-backend/controllers/userSubDocument.controller.js
+++ foodie-pal-backend/controllers/userSubDocument.controller.js
@@ -2,15 +2,31 @@ const User = require("../models/user.model");
 
 const addEditSubDocument = async (req, res) => {
   try {
+    const user = req.user;
     const subDocument = req.body.subDocument;
+
     for (const [key, value] of Object.entries(subDocument)) {
-      await User.findByIdAndUpdate(req.user._id, {
-        [`${key}`]: value,
-      });
+      let updateQuery = {};
+
+      if (key === "items") {
+        const existingItems = user.items || [];
+        const newItem = value;
+
+        if (!existingItems.some((item) => item.name === newItem.name)) {
+          const updatedItems = [...existingItems, newItem];
+          updateQuery[key] = updatedItems;
+        }
+      } else {
+        updateQuery[key] = value;
+      }
+
+      await User.findByIdAndUpdate(req.user._id, updateQuery);
     }
+
     const updatedUser = await User.findById(req.user._id).select(
       "-password -_id -__v"
     );
+
     res.status(200).json({ updatedUser });
   } catch (error) {
     res.status(500).json({ error: error.message });
