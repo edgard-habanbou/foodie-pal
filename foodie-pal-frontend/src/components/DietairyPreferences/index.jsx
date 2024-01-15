a6ce1279f9ee5af0703d42aec4c8c444cb93cd97
edgard <edgard.habanbou@hotmail.com>
2024-01-15 19:27:13 +0200

    Frontend: Added dietairy restrictions

--- foodie-pal-frontend/src/components/DietairyPreferences/index.jsx
+++ foodie-pal-frontend/src/components/DietairyPreferences/index.jsx
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 function DietairyPreferences({ toggleModal }) {
+  const [selectedRestriction, setSelectedRestriction] = useState([]);
+  const restrictions = [
+    "Vegetarian",
+    "Vegan",
+    "Gluten Free",
+    "Dairy-Free",
+    "Nut-Free",
+  ];
+  const handleRestrictions = (e) => {
+    setSelectedRestriction(e.target.value);
+  };
   return (
     <div className="modal-overlay">
       <div className="modal">
         <div className="modal-header">
-          <h2>Modal Title</h2>
-          <button onClick={toggleModal}>&times;</button>
+          <h2>Dietairy Preferences</h2>
+          <button onClick={toggleModal}>
+            <FontAwesomeIcon icon={faX} color="white" />
+          </button>
         </div>
-        <div className="modal-content">
-          <p>Modal content</p>
+        <hr />
+        <div className="modal-content flex gap column">
+          <div className="flex gap column margin ">
+            <div>
+              <h3>Dietary Restrictions</h3>
+              <hr />
+            </div>
+            <div className="flex gap wrap padding">
+              {restrictions.map((restriction) => {
+                return (
+                  <div className="flex center gap" key={restriction}>
+                    <input
+                      onClick={handleRestrictions}
+                      value={restriction}
+                      type="radio"
+                      id={restriction}
+                      name="restriction"
+                    />
+                    <span htmlFor={restriction}>{restriction}</span>
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              <label htmlFor="allergies-profile">
+                Other Allergies (comma separated)
+              </label>
+              <input
+                type="text"
+                className="input"
+                placeholder="Nuts, Dairy...."
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
