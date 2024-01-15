b5ba56077f8b4a57e8ab9bfe47bd75dff0aa0ae5
edgard <edgard.habanbou@hotmail.com>
2024-01-15 18:18:03 +0200

    Frontend: Cleaned profile page

--- foodie-pal-frontend/src/pages/Profile/index.jsx
+++ foodie-pal-frontend/src/pages/Profile/index.jsx
@@ -1,24 +1,12 @@
 import React from "react";
 import Nav from "../../components/Nav";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 function Profile() {
-  const showMenu = () => {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("showing");
-    menu.classList.toggle("hidden");
-  };
   return (
     <div className="flex background">
       <div>
         <Nav />
       </div>
-      <div className="landing">
-        <button onClick={showMenu}>
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-        <h1>Profile</h1>
-      </div>
+      <div className="landing"></div>
     </div>
   );
 }
