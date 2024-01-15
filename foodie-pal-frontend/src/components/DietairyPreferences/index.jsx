2ff85188ca0a13e1403830ecb4f86fe504c87a96
edgard <edgard.habanbou@hotmail.com>
2024-01-15 19:55:09 +0200

    Frontend: Added Information to dietairy resrictions modal

--- foodie-pal-frontend/src/components/DietairyPreferences/index.jsx
+++ foodie-pal-frontend/src/components/DietairyPreferences/index.jsx
@@ -3,7 +3,13 @@ import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
 function DietairyPreferences({ toggleModal }) {
-  const [selectedRestriction, setSelectedRestriction] = useState([]);
+  const [selectedRestriction, setSelectedRestriction] = useState("");
+  const [SelectedFlavors, setSelectedFlavors] = useState([]);
+  const handleSave = () => {
+    console.log(selectedRestriction);
+    console.log(SelectedFlavors);
+    toggleModal();
+  };
   const restrictions = [
     "Vegetarian",
     "Vegan",
@@ -13,6 +19,10 @@ function DietairyPreferences({ toggleModal }) {
   ];
   const handleRestrictions = (e) => {
     setSelectedRestriction(e.target.value);
+  };
+  const flavorPreferences = ["Spicy", "Sweet", "Savory", "Bitter"];
+  const handleFlavorPreferences = (e) => {
+    setSelectedFlavors([...SelectedFlavors, e.target.value]);
   };
   return (
     <div className="modal-overlay">
@@ -26,36 +36,81 @@ function DietairyPreferences({ toggleModal }) {
         <hr />
         <div className="modal-content flex gap column">
           <div className="flex gap column margin ">
-            <div>
-              <h3>Dietary Restrictions</h3>
-              <hr />
+            <div className="flex column gap">
+              <div className="width-fit">
+                <label htmlFor="flavor">Dietary Restrictions</label>
+                <hr />
+              </div>
+              <div className="flex gap wrap ">
+                {restrictions.map((restriction) => {
+                  return (
+                    <div className="flex center gap" key={restriction}>
+                      <input
+                        onClick={handleRestrictions}
+                        value={restriction}
+                        type="radio"
+                        id={restriction}
+                        name="restriction"
+                      />
+                      <span htmlFor={restriction}>{restriction}</span>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-            <div className="flex gap wrap padding">
-              {restrictions.map((restriction) => {
-                return (
-                  <div className="flex center gap" key={restriction}>
-                    <input
-                      onClick={handleRestrictions}
-                      value={restriction}
-                      type="radio"
-                      id={restriction}
-                      name="restriction"
-                    />
-                    <span htmlFor={restriction}>{restriction}</span>
-                  </div>
-                );
-              })}
-            </div>
-            <div>
-              <label htmlFor="allergies-profile">
-                Other Allergies (comma separated)
-              </label>
+            <div className="flex column gap">
+              <div className="width-fit">
+                <label htmlFor="allergies-profile">Other Allergies</label>
+                <hr />
+              </div>
               <input
                 type="text"
                 className="input"
                 placeholder="Nuts, Dairy...."
               />
             </div>
+          </div>
+          <div className="flex gap column">
+            <div className="flex gap column padding">
+              <div className="flex gap column">
+                <div className="width-fit">
+                  <label htmlFor="cuisine-profile">Cuisine Preferences</label>
+                  <hr />
+                </div>
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="e.g., Italian, Asian, Mediterranean"
+                />
+              </div>
+              <div className="flex column gap">
+                <div className="width-fit">
+                  <label htmlFor="flavor">Flavor Preferences</label>
+                  <hr />
+                </div>
+                <div id="flavor" className="flex wrap gap">
+                  {flavorPreferences.map((flavor) => {
+                    return (
+                      <div className="flex center gap" key={flavor}>
+                        <input
+                          onClick={handleFlavorPreferences}
+                          value={flavor}
+                          type="checkbox"
+                          id={flavor}
+                          name="flavor"
+                        />
+                        <span htmlFor={flavor}>{flavor}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex center">
+            <button className="btn" onClick={handleSave}>
+              Save
+            </button>
           </div>
         </div>
       </div>
