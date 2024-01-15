db9fe285b7b8ba71fd85e2889164ce17b66feca0
edgard <edgard.habanbou@hotmail.com>
2024-01-15 18:33:18 +0200

    Frontend: Added User image

--- foodie-pal-frontend/src/pages/Profile/index.jsx
+++ foodie-pal-frontend/src/pages/Profile/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Nav from "../../components/Nav";
 import Header from "../../components/Header";
 function Profile() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   return (
     <div className="flex background">
       <div>
@@ -9,6 +11,15 @@ function Profile() {
       </div>
       <div className="landing">
         <Header />
+        <div className="flex gap center">
+          <div className="flex gap center">
+            <img
+              className="recipe-image-large"
+              src={`${process.env.REACT_APP_BASE_URL}/images/${user.imageUrl}`}
+              alt="profile"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
