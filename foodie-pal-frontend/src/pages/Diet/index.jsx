b4bd198227dae04a822b1ec04020b6948e6e6d57
edgard <edgard.habanbou@hotmail.com>
2024-01-15 18:14:10 +0200

    Frontend: gathered info

--- foodie-pal-frontend/src/pages/Diet/index.jsx
+++ foodie-pal-frontend/src/pages/Diet/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../../components/Nav";
 import Header from "../../components/Header";
 import Questions from "./questions";
@@ -14,7 +14,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBackward, faForward } from "@fortawesome/free-solid-svg-icons";
 
 function Diet() {
-  console.log(Questions);
+  const [formData, setFormData] = useState({});
+
+  const handleInputChange = (sectionName, question, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [sectionName]: {
+        ...(prevData[sectionName] || {}),
+        [question.Question]: value,
+      },
+    }));
+  };
+
+  const sections = Object.entries(Questions);
+
   return (
     <div className="flex background">
       <div>
@@ -31,42 +44,75 @@ function Diet() {
             modules={[Pagination, Navigation]}
             className="mySwiper"
           >
-            {Object.entries(Questions).map(
-              ([sectionName, sectionQuestions], i) => (
-                <SwiperSlide key={i}>
-                  <h3 className="margin">{sectionName}</h3>
-                  {sectionQuestions.map((question, j) => (
-                    <div key={j} className="margin flex padding column gap">
-                      <p>{question.Question}</p>
-                      {question.type === "text" ? (
-                        <input
-                          type="text"
-                          className="input"
-                          placeholder={question.placeholder}
-                        />
-                      ) : question.type === "select" ? (
-                        <select className="input ">
-                          <option value="" disabled selected>
-                            Select Option
+            {sections.map(([sectionName, sectionQuestions], i) => (
+              <SwiperSlide key={i}>
+                <h3 className="margin">{sectionName}</h3>
+                {sectionQuestions.map((question, j) => (
+                  <div key={j} className="margin flex padding column gap">
+                    <p>{question.Question}</p>
+                    {question.type === "text" ? (
+                      <input
+                        type="text"
+                        className="input"
+                        placeholder={question.placeholder}
+                        onChange={(e) =>
+                          handleInputChange(
+                            sectionName,
+                            question,
+                            e.target.value
+                          )
+                        }
+                      />
+                    ) : question.type === "select" ? (
+                      <select
+                        className="input"
+                        onChange={(e) =>
+                          handleInputChange(
+                            sectionName,
+                            question,
+                            e.target.value
+                          )
+                        }
+                      >
+                        <option value="" defaultValue>
+                          Select Option
+                        </option>
+                        {question.options.map((option, o) => (
+                          <option key={o} value={option.value}>
+                            {option.Name}
                           </option>
-                          {question.options.map((option, o) => (
-                            <option key={o} value={option.value}>
-                              {option.Name}
-                            </option>
-                          ))}
-                        </select>
-                      ) : question.type === "number" ? (
-                        <input
-                          className="input"
-                          type="number"
-                          placeholder={question.placeholder}
-                        />
-                      ) : null}
-                    </div>
-                  ))}
-                </SwiperSlide>
-              )
-            )}
+                        ))}
+                      </select>
+                    ) : question.type === "number" ? (
+                      <input
+                        className="input"
+                        type="number"
+                        placeholder={question.placeholder}
+                        onChange={(e) =>
+                          handleInputChange(
+                            sectionName,
+                            question,
+                            e.target.value
+                          )
+                        }
+                      />
+                    ) : null}
+                  </div>
+                ))}
+                {i === sections.length - 1 && (
+                  <div className="flex center ">
+                    <button
+                      className="btn"
+                      onClick={() =>
+                        console.log(JSON.stringify(formData, null, 2))
+                      }
+                    >
+                      Submit
+                    </button>
+                  </div>
+                )}
+              </SwiperSlide>
+            ))}
           </Swiper>
           <div className="flex space-between">
             <button
