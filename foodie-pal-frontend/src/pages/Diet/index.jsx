ce4ed91592c41024e934592c6669d22c6922a265
edgard <edgard.habanbou@hotmail.com>
2024-01-15 15:23:12 +0200

    Frontend: imported Questions

--- foodie-pal-frontend/src/pages/Diet/index.jsx
+++ foodie-pal-frontend/src/pages/Diet/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import Nav from "../../components/Nav";
 import Header from "../../components/Header";
+import Questions from "./questions";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/navigation";
+
 function Diet() {
   return (
     <div className="flex background">
@@ -9,6 +18,26 @@ function Diet() {
       </div>
       <div className="landing">
         <Header />
+        <div className="questions-section">
+          <Swiper
+            pagination={{
+              type: "fraction",
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   );
