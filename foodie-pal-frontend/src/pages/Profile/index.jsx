b1322038087f12309cfbaf90751ebbfb85223a22
edgard <edgard.habanbou@hotmail.com>
2024-01-15 18:18:49 +0200

    Frontend: Added header

--- foodie-pal-frontend/src/pages/Profile/index.jsx
+++ foodie-pal-frontend/src/pages/Profile/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Nav from "../../components/Nav";
+import Header from "../../components/Header";
 function Profile() {
   return (
     <div className="flex background">
       <div>
         <Nav />
       </div>
-      <div className="landing"></div>
+      <div className="landing">
+        <Header />
+      </div>
     </div>
   );
 }
