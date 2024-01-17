caba7e047d971984a62fce6b357663a351c695c4
edgard <edgard.habanbou@hotmail.com>
2024-01-17 17:54:55 +0200

    Backend: Added check if logged in

---
+++ foodie-pal-frontend/src/assets/checkIfLoggedIn.js
@@ -0,0 +1,16 @@
+import { userApi } from "../network/axios";
+
+const checkIfLoggedIn = async () => {
+  if (localStorage.getItem("token")) {
+    const response = await userApi.checkIfLoggedIn({
+      token: localStorage.getItem("token"),
+    });
+    if (!response.user) {
+      return false;
+    } else {
+      localStorage.setItem("user", response);
+      return true;
+    }
+  }
+};
+export default checkIfLoggedIn;
