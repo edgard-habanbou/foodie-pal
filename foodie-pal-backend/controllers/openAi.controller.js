fca5b98d9556dabc0ecec606795a517681196783
edgard <edgard.habanbou@hotmail.com>
2024-01-18 00:20:25 +0200

    Backend: Fixed openAi call

--- foodie-pal-backend/controllers/openAi.controller.js
+++ foodie-pal-backend/controllers/openAi.controller.js
@@ -11,11 +11,11 @@ const getRecipes = async (req, res) => {
         give me 10 recipes 
         the recipes ingredients should only contain the items given
         I might also give you dietairy preferences
-        you should return only a JSON object with this format: [{id:id(from 0 and add 1 for each item), title: recipe name, description:desc, calories: how many calories, time: how much time (45m, 1hr...), instructions:["instruction1", "instruction2"...], ingredients: ["ingredient1", "ingredient2" ...]}]
+        you should return only a JSON object with this format: [{"id":id(from 0 and add 1 for each item), "title": recipe name, "description":desc, "calories": how many calories, "time": how much time (45m, 1hr...), "instructions":["instruction1", "instruction2"...](make sure to add each instruction by itself), "ingredients": ["ingredient1", "ingredient2" ...]}]
         don't add anything else to the object
         items: Potato, Onion, Tomato, Cucumber, Pepper
         dietairy preferences: restrictions:Nut-Free, flavorPreferences: Spicy, Sweet
-        
+        don't give me less than 10 recipes
         give me only the JSON object remove all texts before and after and between it `,
       },
     ],
@@ -23,7 +23,7 @@ const getRecipes = async (req, res) => {
     max_tokens: 3700,
   });
   res.send({
-    message: JSON.parse(chatCompletion.choices[0].message.content),
+    recipes: JSON.parse(chatCompletion.choices[0].message.content),
   });
 };
 module.exports = {
