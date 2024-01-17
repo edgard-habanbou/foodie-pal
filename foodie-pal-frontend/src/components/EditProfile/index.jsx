6f2e034f773974bc0e21714d59717788263ea5fe
edgard <edgard.habanbou@hotmail.com>
2024-01-17 12:25:26 +0200

    Frontend: Added X button to modal

--- foodie-pal-frontend/src/components/EditProfile/index.jsx
+++ foodie-pal-frontend/src/components/EditProfile/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
-function EditProfile() {
+function EditProfile({ toggleModal }) {
   return (
     <div className="modal-overlay">
       <div className="modal">
         <div className="modal-header">
           <h2>Edit Profile</h2>
+          <button onClick={toggleModal}>
+            <FontAwesomeIcon icon={faX} color="white" />
+          </button>
         </div>
         <hr />
         <div className="modal-content flex gap column">
