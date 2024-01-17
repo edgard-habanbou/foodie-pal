109b22eec49f822d59d0078cc3bf10270b7c66bf
edgard <edgard.habanbou@hotmail.com>
2024-01-17 12:23:15 +0200

    Frontend: Made Edit profile component as a modal

--- foodie-pal-frontend/src/components/EditProfile/index.jsx
+++ foodie-pal-frontend/src/components/EditProfile/index.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./index.css";
 
 function EditProfile() {
-  return <div>Edit Profile</div>;
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <div className="modal-header">
+          <h2>Edit Profile</h2>
+        </div>
+        <hr />
+        <div className="modal-content flex gap column">
+          <h1>Modal Content</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EditProfile;
