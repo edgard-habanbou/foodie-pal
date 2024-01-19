b561d1f34c86b85eeaba1c1bf14b16f8b05bb64a
edgard <edgard.habanbou@hotmail.com>
2024-01-20 01:35:57 +0200

    Frontend: Added styling to diet recipes

--- foodie-pal-frontend/src/components/DietPlan/index.jsx
+++ foodie-pal-frontend/src/components/DietPlan/index.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import SwiperCarousel from "../SwiperCarousel";
+import SwiperVertical from "../SwiperVertical";
+import "./index.css";
 
 function DietPlan() {
   const user = JSON.parse(localStorage.getItem("user"));
   const dietPlan = user.DietPlan;
   const mappedDietPlan = Object.entries(dietPlan);
   return (
-    <div className="flex column gap">
-      {mappedDietPlan.map(([meal, recipes], index) => {
-        return (
-          <div className="flex column gap" key={index}>
-            <h3 className="color-white">{meal}</h3>
-            <div className=" flex swiper-div center">
-              <SwiperCarousel recipes={recipes} row={1} />
+    <div className="diet-plan">
+      <SwiperVertical
+        instructions={mappedDietPlan.map(([meal, recipes], index) => {
+          return (
+            <div className="flex column gap" key={index}>
+              <h3 className="color-white">{meal}</h3>
+              <div className=" flex swiper-div center">
+                <SwiperCarousel recipes={recipes} row={1} />
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+        slidesPerView={2}
+      />
     </div>
   );
 }
