b048268eb1ff94969b4ef4266203e33df62b03f6
edgard <edgard.habanbou@hotmail.com>
2024-01-20 01:24:02 +0200

    Frontend: mapping through diet plan recipes

--- foodie-pal-frontend/src/components/DietPlan/index.jsx
+++ foodie-pal-frontend/src/components/DietPlan/index.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
 function DietPlan() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const dietPlan = user.DietPlan;
+  const mappedDietPlan = Object.entries(dietPlan);
+  console.log(dietPlan);
   return (
-    <div className="flex center message-diet column color-white">Diet Plan</div>
+    <div className="flex center message-diet column color-white">
+      {mappedDietPlan.map(([meal, recipes]) => {
+        return (
+          <div className="flex column">
+            <h1 className="color-white">{meal}</h1>
+            {recipes.map((recipe) => {
+              return (
+                <div className="flex">
+                  <p className="color-white">{recipe.title}</p>
+                </div>
+              );
+            })}{" "}
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
