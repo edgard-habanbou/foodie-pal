904540c2d3a5913b6cf668e4975300ba8db63d21
edgard <edgard.habanbou@hotmail.com>
2024-01-20 01:14:36 +0200

    Frontend: Added Diet Plan Component

---
+++ foodie-pal-frontend/src/components/DietPlan/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function DietPlan() {
+  return (
+    <div className="flex center message-diet column color-white">Diet Plan</div>
+  );
+}
+
+export default DietPlan;
