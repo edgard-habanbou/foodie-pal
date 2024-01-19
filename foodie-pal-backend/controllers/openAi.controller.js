4b529f0efdcb69b780f67d3d23d4e5e74fb1bea6
edgard <edgard.habanbou@hotmail.com>
2024-01-19 21:43:47 +0200

    Backend: cleaned code

--- foodie-pal-backend/controllers/openAi.controller.js
+++ foodie-pal-backend/controllers/openAi.controller.js
@@ -24,29 +24,18 @@ const getRecipes = async (req, res) => {
     });
   }
   try {
-    const openai = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `Consider yourself a machine-learning model that returns recipes based on items I will give you.
-          the recipe's ingredients should only contain the items given but you can add spices.
-          give small instructions for example: "Boil the water" or "Cut the onion into cubes".
-          I might also give you dietary preferences
-          You should return only a JSON object with this format: [{"id": id(from 0 and add 1 for each item), "title": recipe name, "description":description, "calories": how many calories, "time": how much time (45m) (don't give me in hours), "instructions":["instruction1", "instruction2"...](make sure to add each instruction by itself), "ingredients": ["ingredient1", "ingredient2" ...]}]
-          Don't add anything else to the object
-          ${items}
-          ${DietairyPreferences}
-          Please make sure that you don't give me less than 10 recipes and don't give me more than 20
-          Give me only the JSON object and remove all texts before and after it.`,
-        },
-      ],
-      model: "gpt-3.5-turbo-1106",
-      max_tokens: 3700,
-    });
-    const recipes = JSON.parse(chatCompletion.choices[0].message.content);
+    const message = `Consider yourself a machine-learning model that returns recipes based on items I will give you.
+    the recipe's ingredients should only contain the items given but you can add spices.
+    give small instructions for example: "Boil the water" or "Cut the onion into cubes".
+    I might also give you dietary preferences
+    You should return only a JSON object with this format: [{"id": id(from 0 and add 1 for each item), "title": recipe name, "description":description, "calories": how many calories, "time": how much time (45m) (don't give me in hours), "instructions":["instruction1", "instruction2"...](make sure to add each instruction by itself), "ingredients": ["ingredient1", "ingredient2" ...]}]
+    Don't add anything else to the object
+    ${items}
+    ${DietairyPreferences}
+    Please make sure that you don't give me less than 10 recipes and don't give me more than 20
+    Give me only the JSON object and remove all texts before and after it.`;
+
+    const recipes = await chatCompletion(message);
 
     const fetchImagePromises = recipes.map(async (recipe, index) => {
       const encodedTitle = encodeURIComponent(recipe.title);
@@ -82,7 +71,52 @@ const getRecipes = async (req, res) => {
   }
 };
 
+const makeDietPlan = async (req, res) => {
+  const user = req.user;
+
+  const DietQuestions = JSON.stringify(user.DietQuestions);
+  const meals = ["breakfast", "lunch", "dinner", "snack"];
+
+  try {
+    meals.forEach(async (meal) => {
+      const message = makeMessage(meal, DietQuestions);
+      const recipes = await chatCompletion(message);
+      user.DietPlan[meal] = recipes;
+    });
+  } catch (e) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getRecipes,
   makeDietPlan,
+};
+
+const makeMessage = (meal, DietQuestions) => {
+  return `
+  Consider yourself a dietitian that gives a diet plan after having questions and their answers
+  I will give you the answers to the questions and you will return a JSON object with the meals for ${meal}
+  your answer MUST BE 10 RECIPIES
+  the JSON object should be in this format: [{"id": id(from 0 and add 1 for each item), "title": recipe name, "description":description, "calories": how many calories, "time": how much time (45m) (don't give me in hours), "instructions":["instruction1", "instruction2"...](make sure to add each instruction by itself), "ingredients": ["ingredient1", "ingredient2" ...]}]
+  Don't add anything else to the object and make sure to remove all texts before and after it.
+  the questions and their answers are: ${DietQuestions}
+  `;
+};
+
+const chatCompletion = async (message) => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: message,
+      },
+    ],
+    model: "gpt-3.5-turbo-1106",
+    max_tokens: 3700,
+  });
+  return JSON.parse(chatCompletion.choices[0].message.content);
 };
