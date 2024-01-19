84dce62a8c5a3774e622600f32fb2577ca0f04ff
edgard <edgard.habanbou@hotmail.com>
2024-01-20 00:58:11 +0200

    Backend: Fixed makeDietPlan

--- foodie-pal-backend/controllers/openAi.controller.js
+++ foodie-pal-backend/controllers/openAi.controller.js
@@ -37,24 +37,37 @@ const makeDietPlan = async (req, res) => {
   const DietQuestions = JSON.stringify(user.DietQuestions);
   const meals = ["breakfast", "lunch", "dinner", "snack"];
   let dietPlans = {};
+  const maxRetries = 3;
 
-  try {
-    for (const meal of meals) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  for (let retry = 0; retry < maxRetries; retry++) {
+    try {
+      for (const meal of meals) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        const message = makeMessage(meal, DietQuestions);
+        const recipes = await chatCompletion(message);
+        const recipesWithImages = await addImages(recipes);
+        dietPlans[meal] = JSON.parse(JSON.stringify(recipesWithImages));
+      }
 
-      const message = makeMessage(meal, DietQuestions);
-      const recipes = await chatCompletion(message);
-      const recipesWithImages = await addImages(recipes);
+      const updatedUser = await User.findByIdAndUpdate(
+        user._id,
+        { DietPlan: dietPlans },
+        { new: true }
+      );
 
-      dietPlans[meal] = JSON.parse(JSON.stringify(recipesWithImages));
+      res.status(200).json({ updatedUser });
+      break;
+    } catch (error) {
+      console.error(error);
+      if (retry < maxRetries - 1) {
+        console.log(`Retrying (${retry + 1}/${maxRetries})...`);
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } else {
+        res
+          .status(500)
+          .json({ error: `Max retries reached: ${error.message}` });
+      }
     }
-    User.findByIdAndUpdate(user._id, { DietPlan: dietPlans });
-    const updatedUser = User.findById(user._id);
-
-    res.status(200).json({ updatedUser });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
   }
 };
 
@@ -68,9 +81,9 @@ const makeMessage = (meal, DietQuestions) => {
   Consider yourself a dietitian that gives a diet plan after having questions and their answers
   I will give you the answers to the questions and you will return a JSON object with the meals for ${meal}
   your answer MUST BE 10 RECIPIES
-  You should return only a JSON object with this format: [{"id": id(from 0 and add 1 for each item), "title": recipe name, "description":description, "calories": how many calories, "time": how much time (45m) (don't give me in hours), "instructions":["instruction1", "instruction2"...](make sure to add each instruction by itself), "ingredients": ["ingredient1", "ingredient2" ...]}]
+  You should return only a JSON object with this format: [{"id": id(from 0 and add 1 for each item), "title": recipe name, "description":description, "calories": how many calories, "time": how much time (45m) (don't give me in hours), "instructions":["instruction1", "instruction2"...](make sure to add each instruction by itself), "ingredients": ["ingredient1", "ingredient2" ...]},{...}]
   Give me only the JSON object and remove all texts before and after it.
-  make sure to close the JSON file at the end
+  DON'T MAKE ERRORS IN THE JSON FILE CLOSE IT AT THE END
   the questions and their answers are: ${DietQuestions}
   `;
 };
@@ -90,29 +103,6 @@ const chatCompletion = async (message) => {
     max_tokens: 3700,
   });
   return JSON.parse(chatCompletion.choices[0].message.content);
-};
-
-const makeItemsAndPreferences = (userItems, pref, category) => {
-  if (category === "All") {
-    category = null;
-  }
-  let items = "items:";
-  if (userItems.length !== 0) {
-    userItems.forEach((item) => {
-      items = items + item.name + ", ";
-    });
-  }
-  let DietairyPreferences = "DietaryPreferences: " + category + ", ";
-  if (pref !== undefined) {
-    const arr = Object.entries(pref._doc);
-
-    arr.forEach((element) => {
-      if (element[0] !== "_id") {
-        DietairyPreferences += element[0] + ":" + element[1] + ", ";
-      }
-    });
-  }
-  return items + " " + DietairyPreferences;
 };
 
 const addImages = async (recipes) => {
@@ -137,7 +127,29 @@ const addImages = async (recipes) => {
   });
 
   await Promise.all(fetchImagePromises);
-  console.log(recipesWithImages);
 
   return recipesWithImages;
+};
+
+const makeItemsAndPreferences = (userItems, pref, category) => {
+  if (category === "All") {
+    category = null;
+  }
+  let items = "items:";
+  if (userItems.length !== 0) {
+    userItems.forEach((item) => {
+      items = items + item.name + ", ";
+    });
+  }
+  let DietairyPreferences = "DietaryPreferences: " + category + ", ";
+  if (pref !== undefined) {
+    const arr = Object.entries(pref._doc);
+
+    arr.forEach((element) => {
+      if (element[0] !== "_id") {
+        DietairyPreferences += element[0] + ":" + element[1] + ", ";
+      }
+    });
+  }
+  return items + " " + DietairyPreferences;
 };
