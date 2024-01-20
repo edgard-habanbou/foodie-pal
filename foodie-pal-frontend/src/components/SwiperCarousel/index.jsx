0ab9bb5627f8b5fd5796fbac7ef0f89ab504aac9
edgard <edgard.habanbou@hotmail.com>
2024-01-20 18:29:01 +0200

    Frontend: made slides per view dynamic

--- foodie-pal-frontend/src/components/SwiperCarousel/index.jsx
+++ foodie-pal-frontend/src/components/SwiperCarousel/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Grid, Pagination } from "swiper/modules";
 import Card from "../Card";
@@ -7,7 +7,35 @@ import "swiper/css/grid";
 import "swiper/css/pagination";
 import "./index.css";
 
-function SwiperCarousel({ recipes, row, slidesPerView = 2 }) {
+function SwiperCarousel({ recipes, row }) {
+  const [slidesPerView, setSlidesPerView] = useState(2);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const calculateSlidesPerView = (width) => {
+    if (width >= 1200) {
+      setSlidesPerView(5);
+    } else if (width >= 992) {
+      setSlidesPerView(4);
+    } else if (width >= 768) {
+      setSlidesPerView(3);
+    } else if (width >= 400) {
+      setSlidesPerView(2);
+    } else {
+      setSlidesPerView(1);
+    }
+  };
+  useEffect(() => {
+    calculateSlidesPerView(screenWidth);
+  }, [screenWidth]);
   return (
     <Swiper
       slidesPerView={slidesPerView}
