7e9b789aedb9251ba2f1707c5fe48ce23843ef07
edgard <edgard.habanbou@hotmail.com>
2024-01-20 17:21:58 +0200

    Frontend: Added questions array

---
+++ foodie-pal-frontend/src/pages/Auth/texts.js
@@ -0,0 +1,30 @@
+const texts = [
+  {
+    tilte: "Welcome to Foodie-Pal!",
+    description:
+      "Embark on a culinary adventure with Foodie-Pal, your personalized cooking companion. From the simplest ingredients to gourmet delights, discover a world of flavors right at your fingertips.",
+  },
+  {
+    title: "Tailored to Your Tastes",
+    description:
+      "Our machine learning algorithms analyze your ingredients and preferences to suggest recipes crafted just for you. Say goodbye to guesswork and hello to a personalized food journey. ",
+  },
+  {
+    title: "Endless Culinary Exploration",
+    description: `Explore a diverse range of dishes from around the world, learn new cooking techniques, and satisfy your cravings. 
+
+      Foodie-Pal is not just an app;  it's your companion for discovering, learning, and creating delicious meals every day.`,
+  },
+  {
+    title: "Cooking Made Easy ",
+    description:
+      "Say goodbye to kitchen stress. Foodie-Pal simplifies the cooking experience. Whether you're a seasoned chef or a novice in the kitchen, our app guides you step by step, making every meal a delightful experience. ",
+  },
+  {
+    title: "Cooking Simplified",
+    description:
+      "Say goodbye to kitchen stress. Foodie-Pal simplifies the cooking experience. Whether you're a seasoned chef or a novice in the kitchen, our app guides you step by step, making every meal a delightful experience.  ",
+  },
+];
+
+export default texts;
