31f8983e666bf49d81a900c04d17cac9d52205a7
edgard <edgard.habanbou@hotmail.com>
2024-01-21 20:43:09 +0200

    Admin: Fixed auth page

--- foodie-pal-admin/src/renderer/src/pages/Auth/index.jsx
+++ foodie-pal-admin/src/renderer/src/pages/Auth/index.jsx
@@ -1,44 +1,44 @@
-import React, { useState } from "react";
-import { userApi } from "../../network/axios";
-import { useNavigate } from "react-router-dom";
-import Loading from "../../components/Loading";
-import Signup from "../Signup";
-import Login from "../Login";
-import ForgotPassword from "../ForgotPassword";
-import { useParams } from "react-router-dom";
+import { useState } from 'react'
+import { userApi } from '../../network/axios'
+import { useNavigate } from 'react-router-dom'
+import Loading from '../../components/Loading'
+import Signup from '../Signup'
+import Login from '../Login'
+import ForgotPassword from '../ForgotPassword'
+import { useParams } from 'react-router-dom'
 
-import "./index.css";
-import ResetPassword from "../ResetPassword";
+import './index.css'
+import ResetPassword from '../ResetPassword'
 
 function Auth() {
-  const [Load, setLoading] = useState(false);
-  const [Register, setRegister] = useState(false);
-  const [forgotPassword, setForgotPassword] = useState(false);
-  const navigate = useNavigate();
+  const [Load, setLoading] = useState(false)
+  const [Register, setRegister] = useState(false)
+  const [forgotPassword, setForgotPassword] = useState(false)
+  const navigate = useNavigate()
 
-  const { token } = useParams();
+  const { token } = useParams()
 
   const handleRegister = (e) => {
-    e.preventDefault();
-    setRegister(!Register);
-  };
+    e.preventDefault()
+    setRegister(!Register)
+  }
 
   const handleForgotPasswordBtn = (e) => {
-    e.preventDefault();
-    setForgotPassword(!forgotPassword);
-  };
+    e.preventDefault()
+    setForgotPassword(!forgotPassword)
+  }
 
   const checkIfLoggedIn = async () => {
-    if (localStorage.getItem("token")) {
+    if (localStorage.getItem('token')) {
       const response = await userApi.checkIfLoggedIn({
-        token: localStorage.getItem("token"),
-      });
+        token: localStorage.getItem('token')
+      })
       if (response.user) {
-        navigate("/home");
+        navigate('/home')
       }
     }
-  };
-  checkIfLoggedIn();
+  }
+  checkIfLoggedIn()
   return (
     <div>
       {Load && <Loading />}
@@ -57,7 +57,7 @@ function Auth() {
         />
       )}
     </div>
-  );
+  )
 }
 
-export default Auth;
+export default Auth
