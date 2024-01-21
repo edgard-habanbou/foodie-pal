1e832d6637fbf56d83b518112f6bddcf83385115
edgard <edgard.habanbou@hotmail.com>
2024-01-21 03:12:52 +0200

    Frontend: Added navigate to profile

--- foodie-pal-frontend/src/components/Header/index.jsx
+++ foodie-pal-frontend/src/components/Header/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
-function header() {
+
+function Header() {
   const userImage = JSON.parse(localStorage.getItem("user")).imageUrl;
   const showMenu = () => {
     const menu = document.getElementById("menu");
@@ -15,14 +16,16 @@ function header() {
         <FontAwesomeIcon icon={faBars} size="xl" />
       </button>
       <div>
-        <img
-          src={`${process.env.REACT_APP_BASE_URL}/images/` + userImage}
-          alt="userImage"
-          className="user-image"
-        />
+        <a href="/profile">
+          <img
+            src={`${process.env.REACT_APP_BASE_URL}/images/` + userImage}
+            alt="userImage"
+            className="user-image"
+          />
+        </a>
       </div>
     </div>
   );
 }
 
-export default header;
+export default Header;
