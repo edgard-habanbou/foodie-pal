c32d91745377673771a5f1fc421f390f56855b42
edgard <edgard.habanbou@hotmail.com>
2024-01-21 20:17:59 +0200

    Admin: Cleaned code

--- foodie-pal-admin/src/renderer/src/main.jsx
+++ foodie-pal-admin/src/renderer/src/main.jsx
@@ -1,10 +1,4 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './assets/index.css'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
