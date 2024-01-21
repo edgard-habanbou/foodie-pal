15b6a1c0a3ee52b4c7aa0161f368b9572d11d9a4
edgard <edgard.habanbou@hotmail.com>
2024-01-21 21:40:50 +0200

    Admin: Fixed axios request urls

--- foodie-pal-admin/src/renderer/src/network/axios.js
+++ foodie-pal-admin/src/renderer/src/network/axios.js
@@ -1,13 +1,10 @@
 import { api } from './base'
 class UserApi {
   async loginUser(user) {
-    return await api.post('/auth/login-admin', user)
-  }
-  async checkToken(token) {
-    return await api.post(`/auth/check-token-admin/`, token)
+    return await api.post('/auth/login/admin', user)
   }
   async checkIfLoggedIn(token) {
-    return await api.post('/auth/check-admin', token)
+    return await api.post('/auth/check/admin', token)
   }
 }
 
