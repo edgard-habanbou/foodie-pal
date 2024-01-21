448d08ac149f23b1c7eb24907c6edd1116ae4a9d
edgard <edgard.habanbou@hotmail.com>
2024-01-21 20:26:58 +0200

    Admin: Added Popup component

---
+++ foodie-pal-admin/src/renderer/src/components/Popup/index.jsx
@@ -0,0 +1,22 @@
+import Swal from "sweetalert2";
+
+export default function Popup({
+  title,
+  text,
+  icon,
+  confirmButtonText,
+  confirmButtonColor,
+  action,
+}) {
+  Swal.fire({
+    title: title,
+    text: text,
+    icon: icon,
+    confirmButtonColor: confirmButtonColor,
+    confirmButtonText: confirmButtonText,
+  }).then((result) => {
+    if (result.isConfirmed && action) {
+      action();
+    }
+  });
+}
