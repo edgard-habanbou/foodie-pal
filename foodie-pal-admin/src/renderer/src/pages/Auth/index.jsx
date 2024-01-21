cca4f50c9eb8f515d3fc4e14f4aa1af0c84607de
edgard <edgard.habanbou@hotmail.com>
2024-01-21 20:46:34 +0200

    Admin: removed unnecessary imports

--- foodie-pal-admin/src/renderer/src/pages/Auth/index.jsx
+++ foodie-pal-admin/src/renderer/src/pages/Auth/index.jsx
@@ -2,31 +2,13 @@ import { useState } from 'react'
 import { userApi } from '../../network/axios'
 import { useNavigate } from 'react-router-dom'
 import Loading from '../../components/Loading'
-import Signup from '../Signup'
 import Login from '../Login'
-import ForgotPassword from '../ForgotPassword'
-import { useParams } from 'react-router-dom'
 
 import './index.css'
-import ResetPassword from '../ResetPassword'
 
 function Auth() {
   const [Load, setLoading] = useState(false)
-  const [Register, setRegister] = useState(false)
-  const [forgotPassword, setForgotPassword] = useState(false)
   const navigate = useNavigate()
-
-  const { token } = useParams()
-
-  const handleRegister = (e) => {
-    e.preventDefault()
-    setRegister(!Register)
-  }
-
-  const handleForgotPasswordBtn = (e) => {
-    e.preventDefault()
-    setForgotPassword(!forgotPassword)
-  }
 
   const checkIfLoggedIn = async () => {
     if (localStorage.getItem('token')) {
@@ -43,19 +25,7 @@ function Auth() {
     <div>
       {Load && <Loading />}
       <div className="image"></div>
-      {Register ? (
-        <Signup handleRegister={handleRegister} setLoading={setLoading} />
-      ) : forgotPassword ? (
-        <ForgotPassword handleForgotPasswordBtn={handleForgotPasswordBtn} />
-      ) : token ? (
-        <ResetPassword token={token} />
-      ) : (
-        <Login
-          handleRegister={handleRegister}
-          handleForgotPasswordBtn={handleForgotPasswordBtn}
-          setLoading={setLoading}
-        />
-      )}
+      <Login setLoading={setLoading} />
     </div>
   )
 }
