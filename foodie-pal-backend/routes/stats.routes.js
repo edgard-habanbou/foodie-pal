f478339ca19d73d94cf2519f30a9c6d871c18076
edgard <edgard.habanbou@hotmail.com>
2024-01-22 14:46:54 +0200

    Backend: Fixed routes not configured right

--- foodie-pal-backend/routes/stats.routes.js
+++ foodie-pal-backend/routes/stats.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getStats } = require("../controllers/stats.controllers");
+const { getStats } = require("../controllers/stats.controller");
 const router = express.Router();
 
 router.post("/getstats", getStats);
