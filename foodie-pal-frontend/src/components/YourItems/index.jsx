f02c97dc41cf7aa1eb94ade2d1518b096669e354
edgard <edgard.habanbou@hotmail.com>
2024-01-22 20:16:55 +0200

    Frontend: Added Your Items to component

--- foodie-pal-frontend/src/components/YourItems/index.jsx
+++ foodie-pal-frontend/src/components/YourItems/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function YourItems() {
-  return <div></div>;
+  return (
+    <>
+      <div className="title">
+        <h4 className="color-white">Your Items</h4>
+        <hr />
+      </div>
+    </>
+  );
 }
 
 export default YourItems;
