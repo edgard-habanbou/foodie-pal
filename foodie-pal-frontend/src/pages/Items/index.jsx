8a4a728676d1592e11851ee56595ec2154db402a
edgard <edgard.habanbou@hotmail.com>
2024-01-22 20:02:49 +0200

    Frontend: Removed add by photo div from items page

--- foodie-pal-frontend/src/pages/Items/index.jsx
+++ foodie-pal-frontend/src/pages/Items/index.jsx
@@ -133,34 +133,6 @@ const Items = () => {
                 <FontAwesomeIcon icon={faCamera} />
               </button>
             </div>
-
-            <div className="flex center column">
-              <div className="camera-items flex">
-                <input
-                  type="file"
-                  name=""
-                  hidden
-                  id="fileInput"
-                  onChange={handleFileChange}
-                />
-                <img
-                  src={
-                    uploadedImage
-                      ? uploadedImage
-                      : `${process.env.REACT_APP_BASE_URL}/default-item.png`
-                  }
-                  className="items-img"
-                  alt="Uploaded"
-                />
-                <div className=" full-width">
-                  <SwiperVertical
-                    itemsInPic={itemsInPic}
-                    slidesPerView={3}
-                    addBtnHandler={addBtnHandler}
-                  />
-                </div>
-              </div>
-            </div>
             <div className="title">
               <h4 className="color-white">Your Items</h4>
               <hr />
