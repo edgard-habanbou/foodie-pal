e78f945640feb2f002fe06c265d2a0685fb24dca
edgard <edgard.habanbou@hotmail.com>
2024-01-22 15:44:43 +0200

    Backend: Added diet plan count and creation times to getstats

--- foodie-pal-backend/controllers/stats.controller.js
+++ foodie-pal-backend/controllers/stats.controller.js
@@ -9,7 +9,14 @@ const getStats = async (req, res) => {
     }
     const { userCount, userCreationTimes } =
       await getuserCountAndCreationTime();
-    res.status(200).send({ role: "admin", userCount, userCreationTimes });
+    const { dietPlanCount, dietPlanCreationTimes } = await getDietPlanCount();
+    res.status(200).send({
+      role: "admin",
+      userCount,
+      userCreationTimes,
+      dietPlanCount,
+      dietPlanCreationTimes,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Internal Server Error" });
@@ -27,4 +34,23 @@ const getuserCountAndCreationTime = async () => {
   const userCount = users.length;
   const userCreationTimes = users.map((user) => user.createdAt);
   return { userCount, userCreationTimes };
+};
+
+const getDietPlanCount = async () => {
+  try {
+    const usersWithDietPlan = await User.find({
+      DietPlan: { $exists: true, $ne: null },
+    }).select("createdAt DietPlan firstName lastName");
+    const dietPlanCount = usersWithDietPlan.length;
+    const dietPlanCreationTimes = usersWithDietPlan.map((user) => {
+      return {
+        userId: user.firstName + " " + user.lastName,
+        createdAt: user.DietPlan.createdAt,
+      };
+    });
+
+    return { dietPlanCount, dietPlanCreationTimes };
+  } catch (error) {
+    console.error("Error in getDietPlanCount:", error);
+  }
 };
