7eeb54dac446dbb938b6ba7a2c2bc84e0555c2a1
edgard <edgard.habanbou@hotmail.com>
2024-01-22 20:27:19 +0200

    Frontend: Added inputs to YourItems component

--- foodie-pal-frontend/src/components/AddItem/index.jsx
+++ foodie-pal-frontend/src/components/AddItem/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function AddItem() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex margin gap  center">
+        <input
+          type="text"
+          className="input"
+          placeholder="Item Name"
+          value={itemName}
+          onChange={(e) => {
+            setItemName(e.target.value);
+          }}
+        />
+        <button
+          className="btn"
+          onClick={() => {
+            addBtnHandler(itemName);
+            setItemName("");
+          }}
+        >
+          Add Item
+        </button>
+        <button className="btn" onClick={handleCamerabtn}>
+          <FontAwesomeIcon icon={faCamera} />
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default AddItem;
