cf2e34391163d38643b97709c7c4a83f206d1960
edgard <edgard.habanbou@hotmail.com>
2024-01-22 15:20:34 +0200

    Backend: Added user count to getstats

--- foodie-pal-backend/controllers/stats.controller.js
+++ foodie-pal-backend/controllers/stats.controller.js
@@ -1,17 +1,28 @@
 const User = require("../models/user.model");
-const mongoose = require("mongoose");
 const { ObjectId } = require("mongodb");
 
 const getStats = async (req, res) => {
-  if (req.user.userRole.equals(new ObjectId(process.env.ADMIN_ID))) {
-    res.status(200).send({ role: "admin" });
-    return;
-  } else {
-    res.status(400).send({ message: "Not Authorized" });
-    return;
+  try {
+    if (!req.user.userRole.equals(new ObjectId(process.env.ADMIN_ID))) {
+      res.status(403).send({ message: "Not Authorized" });
+      return;
+    }
+    const { userCount } = await getuserCount();
+    res.status(200).send({ role: "admin", userCount });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
   }
 };
 
 module.exports = {
   getStats,
+};
+const getuserCount = async () => {
+  const startDate = new Date(0);
+  const users = await User.find({
+    createdAt: { $gte: startDate },
+  }).select("createdAt");
+  const userCount = users.length;
+  return { userCount };
 };
