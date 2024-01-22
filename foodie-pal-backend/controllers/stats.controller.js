e94dfcd260f0b6fdff5a26a594f9142e9032ac54
edgard <edgard.habanbou@hotmail.com>
2024-01-22 14:55:52 +0200

    Backend: Added validation to check if admin in getStats

--- foodie-pal-backend/controllers/stats.controller.js
+++ foodie-pal-backend/controllers/stats.controller.js
@@ -1,7 +1,16 @@
 const User = require("../models/user.model");
 const mongoose = require("mongoose");
+const { ObjectId } = require("mongodb");
 
-const getStats = async (req, res) => {};
+const getStats = async (req, res) => {
+  if (req.user.userRole.equals(new ObjectId(process.env.ADMIN_ID))) {
+    res.status(200).send({ role: "admin" });
+    return;
+  } else {
+    res.status(400).send({ message: "Not Authorized" });
+    return;
+  }
+};
 
 module.exports = {
   getStats,
