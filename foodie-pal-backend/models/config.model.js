1cadcc3b3a2d2148b3d3fa77a5c4878c871fe941
edgard <edgard.habanbou@hotmail.com>
2024-01-24 12:32:20 +0200

    Backend: Added config model

--- foodie-pal-backend/models/config.model.js
+++ foodie-pal-backend/models/config.model.js
@@ -1,7 +1,48 @@
 const mongoose = require("mongoose");
 
 const ConfigSchema = new mongoose.Schema(
-  {},
+  {
+    MONGODB_URL: {
+      type: String,
+      required: true,
+    },
+    JWT_SECRET: {
+      type: String,
+      required: true,
+    },
+    SERVER_LINK: {
+      type: String,
+      required: true,
+    },
+    CLIENT_LINK: {
+      type: String,
+      required: true,
+    },
+    EMAIL_ADDRESS: {
+      type: String,
+      required: true,
+    },
+    EMAIL_PASSWORD: {
+      type: String,
+      required: true,
+    },
+    CLAIRIFY_API_KEY: {
+      type: String,
+      required: true,
+    },
+    OPENAI_API_KEY: {
+      type: String,
+      required: true,
+    },
+    BING_API_KEY: {
+      type: String,
+      required: true,
+    },
+    ADMIN_ID: {
+      type: String,
+      required: true,
+    },
+  },
   {
     timestamps: true,
   }
