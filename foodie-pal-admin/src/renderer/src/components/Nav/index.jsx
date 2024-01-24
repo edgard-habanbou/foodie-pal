fc886251117be366a4aedbbbabb806d50cade3b4
edgard <edgard.habanbou@hotmail.com>
2024-01-24 11:11:07 +0200

    Admin: removed unused icons

--- foodie-pal-admin/src/renderer/src/components/Nav/index.jsx
+++ foodie-pal-admin/src/renderer/src/components/Nav/index.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import Logo from "../../assets/svgs/Logo.svg";
-import "./index.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
-import { faSeedling } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect } from 'react'
+import Logo from '../../assets/svgs/Logo.svg'
+import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
+import { faSeedling } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 function Nav() {
   const selected = () => {
-    const url = window.location.pathname;
-    const menuItems = document.querySelectorAll(".menu-item");
+    const url = window.location.pathname
+    const menuItems = document.querySelectorAll('.menu-item')
     menuItems.forEach((item) => {
       if (url === item.children[0].pathname) {
-        item.classList.add("selected");
+        item.classList.add('selected')
       } else {
-        item.classList.remove("selected");
+        item.classList.remove('selected')
       }
-    });
-  };
+    })
+  }
   const Logout = () => {
-    localStorage.clear();
-  };
+    localStorage.clear()
+  }
   useEffect(() => {
-    selected();
-  }, []);
+    selected()
+  }, [])
   return (
     <div id="menu" className="menu hidden full-height-screen">
       <div className="flex center full-width">
@@ -39,30 +39,6 @@ function Nav() {
           </a>
           <p>Home</p>
         </div>
-        <div className="menu-item flex column center">
-          <a href="/favorites">
-            <FontAwesomeIcon icon={faHeart} />
-          </a>
-          <p>Fav</p>
-        </div>
-        <div className="menu-item flex column center">
-          <a href="/items">
-            <FontAwesomeIcon icon={faBasketShopping} />
-          </a>
-          <p>Items</p>
-        </div>
-        <div className="menu-item flex column center">
-          <a href="/Diet">
-            <FontAwesomeIcon icon={faSeedling} />
-          </a>
-          <p>Diet</p>
-        </div>
-        <div className="menu-item flex column center">
-          <a href="/profile">
-            <FontAwesomeIcon icon={faUser} />
-          </a>
-          <p>Profile</p>
-        </div>
         <div className="menu-item flex column center logout">
           <a href="/" onClick={Logout}>
             <FontAwesomeIcon icon={faRightFromBracket} />
@@ -72,7 +48,7 @@ function Nav() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
