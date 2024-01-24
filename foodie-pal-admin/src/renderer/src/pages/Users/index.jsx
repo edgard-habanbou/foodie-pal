d0340ca7c8b0e29ca91bcb6aeeada5321d4e1527
edgard <edgard.habanbou@hotmail.com>
2024-01-25 00:10:54 +0200

    Admin: Added users table

--- foodie-pal-admin/src/renderer/src/pages/Users/index.jsx
+++ foodie-pal-admin/src/renderer/src/pages/Users/index.jsx
@@ -5,14 +5,29 @@ import { userApi } from '../../network/axios'
 
 function Users() {
   const [allUsers, setAllUsers] = useState([])
+
   const getAllUsers = async () => {
-    const users = await userApi.getUsers()
-    console.log(users)
-    setAllUsers(users)
+    try {
+      const users = await userApi.getUsers()
+      setAllUsers(users)
+    } catch (error) {
+      console.error('Error fetching users:', error)
+    }
   }
+
+  const handleDeleteUser = async (userId) => {
+    // try {
+    //   await userApi.deleteUser(userId)
+    //   setAllUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId))
+    // } catch (error) {
+    //   console.error('Error deleting user:', error)
+    // }
+  }
+
   useEffect(() => {
     getAllUsers()
   }, [])
+
   return (
     <div className="flex background">
       <div>
@@ -20,10 +35,31 @@ function Users() {
       </div>
       <div className="landing-page full-width flex column gap">
         <Header />
-        {allUsers &&
-          allUsers.map((user) => {
-            return user.firstName
-          })}
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Gender</th>
+              <th>Diet Plan</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUsers &&
+              allUsers.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.firstName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.gender ? 'Female' : 'Male'}</td>
+                  <td>{user.dietPlan ? 'True' : 'False'}</td>
+                  <td>
+                    <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
