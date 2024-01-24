61cc9c12283d200d4d707265f892d1c191fee243
edgard <edgard.habanbou@hotmail.com>
2024-01-25 00:28:59 +0200

    Admin: Added swiper vertical component

---
+++ foodie-pal-admin/src/renderer/src/components/SwiperVertical/index.jsx
@@ -0,0 +1,124 @@
+import { Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import './index.css'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import SwiperCarousel from '../SwiperCarousel'
+import PropTypes from 'prop-types'
+
+function SwiperVertical({
+  instructions,
+  mappedDietPlan,
+  items,
+  itemsInPic,
+  chats,
+  users,
+  slidesPerView = 4.5,
+  addBtnHandler = () => {},
+  deleteBtnHandler = () => {}
+}) {
+  return (
+    <Swiper
+      direction={'vertical'}
+      slidesPerView={slidesPerView}
+      pagination={{
+        clickable: true
+      }}
+      modules={[Pagination]}
+      className={mappedDietPlan ? 'mapped-diet-plan' : 'instructions'}
+    >
+      {mappedDietPlan?.map(([meal, recipes], index) => {
+        if (meal !== 'createdAt' && meal !== 'updatedAt') {
+          return (
+            <SwiperSlide
+              key={index}
+              onClick={() => {
+                localStorage.setItem('recipes', JSON.stringify(recipes))
+              }}
+            >
+              <div className="flex column margin">
+                <div className="padding margin">
+                  <h3 className="color-white">{meal.charAt(0).toUpperCase() + meal.slice(1)}</h3>
+                  <hr />
+                </div>
+                <div className=" flex swiper-div center">
+                  <SwiperCarousel slidesPerView={1.8} recipes={recipes} row={1} />
+                </div>
+              </div>
+            </SwiperSlide>
+          )
+        } else {
+          return null
+        }
+      })}
+      {instructions?.map((instruction, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="instruction">{instruction}</div>
+          </SwiperSlide>
+        )
+      })}
+      {items?.map((item, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="instruction flex space-between">
+              {item.name}
+              <button className="btn-delete" onClick={() => deleteBtnHandler(item.name)}>
+                <FontAwesomeIcon icon={faTrash} size="xl" />
+              </button>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+      {users?.map((user, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="instruction flex space-between">
+              {user.firstName} {user.lastName}
+              <button className="btn-delete" onClick={() => deleteBtnHandler(user._id)}>
+                <FontAwesomeIcon icon={faTrash} size="xl" />
+              </button>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+      {itemsInPic?.map((itemInPic, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="items-in-pic flex space-between">
+              <div>{itemInPic}</div>
+              <div>
+                <button className="btn-add" onClick={() => addBtnHandler(itemInPic)}>
+                  <FontAwesomeIcon icon={faPlus} size="2xl" />
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+      {chats?.map(({ sender, chat }, i) => (
+        <SwiperSlide key={i}>
+          <div className={sender !== 'openai' ? 'flex right' : ''}>
+            <div className={sender !== 'openai' ? 'message-user' : 'message'}>{chat && chat}</div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
+
+SwiperVertical.propTypes = {
+  instructions: PropTypes.array,
+  mappedDietPlan: PropTypes.array,
+  items: PropTypes.array,
+  itemsInPic: PropTypes.array,
+  chats: PropTypes.array,
+  users: PropTypes.array,
+  slidesPerView: PropTypes.number,
+  addBtnHandler: PropTypes.func,
+  deleteBtnHandler: PropTypes.func
+}
+export default SwiperVertical
