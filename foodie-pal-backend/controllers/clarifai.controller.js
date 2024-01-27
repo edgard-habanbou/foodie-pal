edd272ab05cb8174748b6b20520976a5dbb6f698
edgard <edgard.habanbou@hotmail.com>
2024-01-27 14:49:49 +0200

    Backend: Fixed add items by photo

--- foodie-pal-backend/controllers/clarifai.controller.js
+++ foodie-pal-backend/controllers/clarifai.controller.js
@@ -1,26 +1,29 @@
 const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
 
 const getImageURL = async (req, res) => {
   try {
-    const base64Data = req.body.image.replace(/^data:image\/\w+;base64,/, "");
-    const dataBuffer = Buffer.from(base64Data, "base64");
+    const { image } = req.files;
 
-    const imageName = `${Date.now()}.png`;
+    if (!image) return res.sendStatus(400);
+
+    const lastIndex = image.name.lastIndexOf(".");
+    const extention = image.name.slice(lastIndex + 1);
+    const imageName = Date.now() + "." + extention;
+
+    if (extention !== "png" && extention !== "jpg" && extention !== "jpeg") {
+      return res.status(400).send({ message: "invalid image format" });
+    }
 
     const { dirname } = require("path");
     const appDir = dirname(require.main.filename);
     const image_dir = appDir + "/public/items/" + imageName;
-
-    fs.writeFileSync(image_dir, dataBuffer);
+    image.mv(image_dir);
     const items = await getConcepts(
       `${process.env.SERVER_LINK}/items/${imageName}`
     );
-    res.status(200).json({ items });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(200).json({ items: items, image: imageName });
+  } catch (e) {
+    console.log(e);
   }
 };
 
@@ -50,12 +53,6 @@ const getConcepts = async (url) => {
     "Content-Type": "application/json",
   };
 
-  axios
-    .post(apiUrl, requestData, { headers })
-    .then((response) => {
-      return response.data.outputs[0].data.concepts;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  const items = await axios.post(apiUrl, requestData, { headers });
+  return items.data.outputs[0].data.concepts;
 };
